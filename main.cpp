12f9de8bd2d81a87fae4c068cf2f26c2d75b3002
Rishin Doshi <rishdosh@Rishins-iMac.local>
2019-04-11 16:31:42 -0400

    created arrange models fn

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <algorithm>
 
 #include "board.h"
 #include "utility.h"
@@ -15,26 +16,29 @@ int main(int argc, char** argv) {
   Model m_b(2, 4, 'b');
   Model m_c(2, 2, 'c');
   Model m_d(2, 1, 'd');
-  Model m_e(3, 3, 'e');
+
+  vector<Model> models;
+  models.push_back(m_a);
+  models.push_back(m_b);
+  models.push_back(m_c);
+  models.push_back(m_d);
 
   BoardPlate bp(10, 8);
-
-  bp.addModel(m_a);
-  bp.printBoard();
   
-  bp.addModel(m_b);
-  bp.printBoard();
-
-  bp.addModel(m_c);
-  bp.printBoard();
-  
-  bp.addModel(m_d);
-  bp.printBoard();
+  arrangeModels(bp, models);
   
 	return 0;
 }
 
-
 void arrangeModels(BoardPlate& bp, vector<Model>& models) {
+  sort(models.begin(), models.end(), [&](Model& a, Model& b) {
+    return (a.width * a.height) > (b.width * b.height);
+  });
+
+  for (auto& m : models) {
+    bp.addModel(m);
+  }
+
+  bp.printBoard();
 }
 
